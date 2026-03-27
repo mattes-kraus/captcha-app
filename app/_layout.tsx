f6051930a1b3166b55a1035db1246381a4fc1c79
Mattes Kraus <mattes.kraus@uni-konstanz.de>
2026-03-27 16:20:33 +0100

    removed index header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+      {/* Versteckt den Header nur für die index-Seite */}
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
 }
